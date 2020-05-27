4176c70485a55a61fed84d38e0162266817e4be9
Antonio Lazaro Borges <antonio.lazaro@gmail.com>
2020-05-27 20:42:24 -0300

    ajuste servidor web go

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go-lang-labs
+
+Built based on https://codelabs.developers.google.com/codelabs/go-web-server/index.html?index=..%2F..index#4
+
+curl localhost:8080/v1/profile 
+curl localhost:8080/v1/posts
+curl localhost:8080/v2/profile
+curl localhost:8080/v2/posts
--- web-server/main.go
+++ web-server/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 				"encoding/json"
-				"fmt"
 				"log"
+				"fmt"
         "net/http"
 )
 
@@ -13,16 +13,44 @@ type profile struct {
 }
 
 func main() {
-				http.HandleFunc("/welcome", welcome)
+	mux := http.NewServeMux()
 
-				fs := http.FileServer(http.Dir("static"))
-				http.Handle("/", fs)
+	v1Mux := http.NewServeMux()
 
-				http.HandleFunc("/profile", getProfile)
-				
-				if err := http.ListenAndServe(":8080", nil); err != nil {
-					log.Fatal(err)
+	v1Mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+					fmt.Fprintln(w, "v1 Profile")
+	})
+
+	v1Mux.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+					fmt.Fprintln(w, "v1 Posts")
+	})
+
+	v2Mux := http.NewServeMux()
+
+	v2Mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+					fmt.Fprintln(w, "v2 Profile")
+	})
+
+	v2Mux.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+					fmt.Fprintln(w, "v2 Posts")
+	})
+
+	mux.Handle("/v1/", http.StripPrefix("/v1", v1Mux))
+	mux.Handle("/v2/", http.StripPrefix("/v2", v2Mux))
+
+	loggedHandler := loggingMiddleware(mux)
+
+	if err := http.ListenAndServe(":8080", loggedHandler); err != nil {
+					fmt.Println(err)
 	}
+}
+
+func loggingMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					log.Printf("Got a %s request for: %v\n", r.Method, r.URL)
+					handler.ServeHTTP(w, r)
+					log.Printf("Handler finished processing request")
+	})
 }
 
 
